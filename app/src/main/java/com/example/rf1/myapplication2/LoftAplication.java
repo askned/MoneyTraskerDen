b86eff6250295295e1af9cf90fa49f515658a75c
Den <askden@ukr.net>
2015-05-25 00:45:09 +0300

    errors

--- app/src/main/java/com/example/rf1/myapplication2/LoftAplication.java
+++ app/src/main/java/com/example/rf1/myapplication2/LoftAplication.java
@@ -1,9 +1,7 @@
 package com.example.rf1.myapplication2;
 
-import android.app.Application;
+import com.activeandroid.app.Application;
 
-/**
- * Created by 1 on 24.05.2015.
- */
+
 public class LoftAplication extends Application {
 }
