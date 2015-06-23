ff1360064928cde6e25002274dfdb4ec00e7bf96
Denis <askned@users.noreply.github.com>
2015-06-23 17:10:29 +0300

    Update TransactionFragment.java

--- app/src/main/java/com/example/rf1/myapplication2/TransactionFragment.java
+++ app/src/main/java/com/example/rf1/myapplication2/TransactionFragment.java
@@ -62,6 +62,22 @@ public class TransactionFragment extends Fragment {
             }
         });
     }
+       ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.removeItem(viewHolder.getAdapterPosition());
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+    }
 
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
