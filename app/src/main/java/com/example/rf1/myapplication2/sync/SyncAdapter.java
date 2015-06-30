cbcc8106123be6d905b60dfb8498dd86815ca01e
Denis <askned@users.noreply.github.com>
2015-06-30 18:23:15 +0300

    Update SyncAdapter.java

--- app/src/main/java/com/example/rf1/myapplication2/sync/SyncAdapter.java
+++ app/src/main/java/com/example/rf1/myapplication2/sync/SyncAdapter.java
@@ -12,25 +12,32 @@ import com.example.rf1.myapplication2.rest.RestClient;
 import com.example.rf1.myapplication2.rest.RestClient_;
 
 
+@EBean
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
     public SyncAdapter(Context context) {
         super(context, true);
     }
 
+    @RestService
+    RestClient api;
+
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         try {
-            final RestClient api = new RestClient_(getContext());
-            for (Transaction transaction : Transaction.getUnsynced()) {
-                api.addTransactions(transaction.sum, transaction.title, transaction.date.toString());
+            final List<Transaction> unsyncedTransactions = Transaction.getUnsynced();
+            for (Transaction transaction : unsyncedTransactions) {
+                api.addTransaction(transaction.sum, transaction.comment, transaction.trDate.toString());
                 transaction.markSynced();
             }
 
-            for (Transaction transaction : api.getTransactions().data)
+            final Transaction[] serverTransactions = api.getTransactions().data;
+            for (Transaction transaction : serverTransactions)
                 if (!transaction.isInDatabase())
                     transaction.save();
 
-            for (Transaction transaction : Transaction.getSynced())
+            final List<Transaction> syncedTransactions = Transaction.getSynced();
+            for (Transaction transaction : syncedTransactions)
                 transaction.delete();
         } catch (Exception e) {
             e.printStackTrace();
