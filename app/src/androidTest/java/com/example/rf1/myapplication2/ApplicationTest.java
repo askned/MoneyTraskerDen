ed23c5df7c612a65aff6284fa740d42cb8c6b05a
Denis <askned@users.noreply.github.com>
2015-07-01 09:16:57 +0300

    Update ApplicationTest.java

--- app/src/androidTest/java/com/example/rf1/myapplication2/ApplicationTest.java
+++ app/src/androidTest/java/com/example/rf1/myapplication2/ApplicationTest.java
@@ -1,13 +1,39 @@
 package com.example.rf1.myapplication2;
+import android.support.test.espresso.ViewInteraction;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.SmallTest;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import com.example.rf1.myapplication2.R;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
+
+
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity_> {
+
+    public LoginActivityTest() {
+        super(LoginActivity_.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    @SmallTest
+    public void testLoginButtonState() {
+        okButton().check(matches(not(isEnabled())));
+        onView(withHint(R.string.enter_login)).perform(typeText("1"));
+        onView(withHint(R.string.enter_password)).perform(typeText("1"));
+        okButton().check(matches(isEnabled()));
+    }
+
+    private ViewInteraction okButton() {
+        return onView(withText(R.string.action_sign_in));
     }
 }
