fa872174ac489e0fb0006f5aeca034c90ef819a9
Denis <askned@users.noreply.github.com>
2015-07-14 17:20:32 +0300

    Update AddTransactionActivity.java

--- app/src/main/java/com/example/rf1/myapplication2/AddTransactionActivity.java
+++ app/src/main/java/com/example/rf1/myapplication2/AddTransactionActivity.java
@@ -20,9 +20,20 @@ import org.androidannotations.annotations.res.StringArrayRes;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
 
+import com.fourmob.datetimepicker.date.DatePickerDialog;
+import com.fourmob.datetimepicker.date.DatePickerDialog.OnDateSetListener;
+import com.sleepbot.datetimepicker.time.RadialPickerLayout;
+import com.sleepbot.datetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+
 
 @EActivity(R.layout.addtrans)
 public class AddTransactionActivity extends ActionBarActivity {
+    
+      public static final String DATEPICKER_TAG = "datepicker";
+    public static final String TIMEPICKER_TAG = "timepicker";
+
 
     @ViewById
     Toolbar toolbar;
@@ -47,6 +58,29 @@ public class AddTransactionActivity extends ActionBarActivity {
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     //    setHintFromLastTransaction();
+    
+       final Calendar calendar = Calendar.getInstance();
+
+        final DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), isVibrate());
+        final TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY) ,calendar.get(Calendar.MINUTE), false, false);
+
+        findViewById(R.id.dateButton).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.setVibrate(isVibrate());
+                datePickerDialog.setYearRange(1985, 2028);
+                datePickerDialog.setCloseOnSingleTapDay(isCloseOnSingleTapDay());
+                datePickerDialog.show(getSupportFragmentManager(), DATEPICKER_TAG);
+            }
+        });
+
+        findViewById(R.id.timeButton).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePickerDialog.setVibrate(isVibrate());
+                timePickerDialog.setCloseOnSingleTapMinute(isCloseOnSingleTapMinute());
+                timePickerDialog.show(getSupportFragmentManager(), TIMEPICKER_TAG);
     }
 
     @Click
@@ -90,6 +124,40 @@ public class AddTransactionActivity extends ActionBarActivity {
   //String[] ITEMS = getResources().getStringArray(R.array.countries_list);
   //  spinner = (MaterialSpinner) findViewById(R.id.spinner);
    
+    if (savedInstanceState != null) {
+            DatePickerDialog dpd = (DatePickerDialog) getSupportFragmentManager().findFragmentByTag(DATEPICKER_TAG);
+            if (dpd != null) {
+                dpd.setOnDateSetListener(this);
+            }
+
+            TimePickerDialog tpd = (TimePickerDialog) getSupportFragmentManager().findFragmentByTag(TIMEPICKER_TAG);
+            if (tpd != null) {
+                tpd.setOnTimeSetListener(this);
+            }
+        }
+    }
+
+    private boolean isVibrate() {
+        return ((CheckBox) findViewById(R.id.checkBoxVibrate)).isChecked();
+    }
+
+    private boolean isCloseOnSingleTapDay() {
+        return ((CheckBox) findViewById(R.id.checkBoxCloseOnSingleTapDay)).isChecked();
+    }
+
+    private boolean isCloseOnSingleTapMinute() {
+        return ((CheckBox) findViewById(R.id.checkBoxCloseOnSingleTapMinute)).isChecked();
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog datePickerDialog, int year, int month, int day) {
+        Toast.makeText(MainActivity.this, "new date:" + year + "-" + month + "-" + day, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        Toast.makeText(MainActivity.this, "new time:" + hourOfDay + "-" + minute, Toast.LENGTH_LONG).show();
+    }
 
 }
 
