0d7fa0753fcf68eb3c6f278a521eed4df185c2e7
Администратор <zup668665@uk.net>
2015-07-17 01:51:06 +0300

    add category

--- app/src/main/java/com/example/rf1/myapplication2/Category.java
+++ app/src/main/java/com/example/rf1/myapplication2/Category.java
@@ -6,7 +6,6 @@ import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Select;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Table(name = "DBCategory")
@@ -27,19 +26,10 @@ public class Category extends Model {
     public static List<Category> getAll() {
         return new Select()
                 .from(Category.class)
-
+                .orderBy("date DESC")
                 .execute();
 
     }
 
-    public void loadcategorii(){
-         ArrayList<String> ar = new ArrayList<>();
-        String s1 = "Food";
-        String s2 = "Entertaimnet";
-        String s3 = "Other";
-        ar.add(s1);
-        ar.add(s2);
-        ar.add(s3);
-    }
 
 }
