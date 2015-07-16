e19a9ccad0e07710a86b983f7847df9cf6f8c808
Denis <askned@users.noreply.github.com>
2015-07-16 16:47:49 +0300

    Update StatmentFragment.java

--- app/src/main/java/com/example/rf1/myapplication2/StatmentFragment.java
+++ app/src/main/java/com/example/rf1/myapplication2/StatmentFragment.java
@@ -9,19 +9,25 @@ import org.androidannotations.annotations.ViewById;
 @EFragment(R.layout.statistics)
 public class StatmentFragment extends Fragment {
 
-    float[] datapoints = {
-            50, 60, 456, 345, 120
-    };
-
-    @ViewById(R.id.piechart)
-    PieChartView pieChartView;
-
+    Map<String, Float> map = new HashMap<>();
 
     @AfterViews
     void ready() {
-        pieChartView.setDatapoints(datapoints);
+        map.put("Food", 15.7f);
+        map.put("Closes", 35.0f);
+        map.put("Electricity", 25.0f);
+        map.put("Rent", 9.1f);
+        Random r;
+        for (Map.Entry<String, Float> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Float value = entry.getValue();
+
+            r = new Random();
+            int color = Color.argb(100, r.nextInt(256), r.nextInt(256), r.nextInt(256));
+            chart.addPieSlice(new PieModel(key, value, color));
+        }
+        chart.startAnimation();
     }
 
-    }
-
+}
 
