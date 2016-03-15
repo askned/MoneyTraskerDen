20bb858698ac215f7ae698d72460597ef601259a
Denis <askned@users.noreply.github.com>
2016-03-15 14:07:07 +0200

    Update TransactionsResult.java

--- app/src/main/java/com/example/rf1/myapplication2/rest/TransactionsResult.java
+++ app/src/main/java/com/example/rf1/myapplication2/rest/TransactionsResult.java
@@ -2,9 +2,6 @@ package com.example.rf1.myapplication2.rest;
 
 import com.example.rf1.myapplication2.Transaction;
 
-/**
- * Created by 1 on 29.05.2015.
- */
 public class TransactionsResult extends Result {
    public Transaction[] data;
 }
